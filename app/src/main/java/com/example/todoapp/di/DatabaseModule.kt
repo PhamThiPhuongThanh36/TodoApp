d7cee169c8e3104f5c4119c141c337c6d0ea496c
PhamThiPhuongThanh36 <phamthiphuongthanh36@gmail.com>
2025-09-13 23:45:44 +0700

    add hilt

---
+++ app/src/main/java/com/example/todoapp/di/DatabaseModule.kt
@@ -0,0 +1,38 @@
+package com.example.todoapp.di
+
+import android.app.Application
+import android.content.Context
+import androidx.room.Room
+import com.example.todoapp.database.AppDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+    @Provides
+    @Singleton
+    fun provideDatabase(context: Context): AppDatabase {
+        return Room.databaseBuilder(
+            context,
+            AppDatabase::class.java,
+            "todo_database"
+        ).build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideProjectDao(database: AppDatabase) = database.projectDao()
+
+    @Singleton
+    @Provides
+    fun provideListDao(database: AppDatabase) = database.listDao()
+
+    @Singleton
+    @Provides
+    fun provideTaskDao(database: AppDatabase) = database.taskDao()
+
+}
