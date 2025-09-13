25318b82afa433f3d0a90609c1d9e937bcb7e0d7
PhamThiPhuongThanh36 <phamthiphuongthanh36@gmail.com>
2025-09-13 23:57:39 +0700

    add DrawerActivity.kt

--- app/src/main/java/com/example/todoapp/ui/drawer/DrawerActivity.kt
+++ app/src/main/java/com/example/todoapp/ui/drawer/DrawerActivity.kt
@@ -1,13 +1,13 @@
 package com.example.todoapp.ui.drawer
 
 import androidx.compose.foundation.Image
-import com.example.todoapp.R
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,13 +20,25 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.todoapp.R
+import com.example.todoapp.ui.common.OperationCustom
 
 @Composable
-fun DrawerScreen() {
+fun DrawerScreen(
+    projectSection: @Composable () -> Unit?,
+    onAddNewProject: () -> Unit,
+    onEditProject: () -> Unit,
+    onProjectViewByTag: () -> Unit,
+    onProjectViewByDueDate: () -> Unit,
+    onPomodoroMode: () -> Unit,
+    onTag: () -> Unit,
+    onMoveProject: () -> Unit,
+    onDeletedProjectView: () -> Unit
+) {
     Column(
         modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
+            .fillMaxWidth(0.85f)
+            .fillMaxHeight()
             .background(Color(0xFFFFFFFF))
     ) {
         Row(
@@ -74,11 +86,76 @@ fun DrawerScreen() {
                 contentDescription = "log out"
             )
         }
+        projectSection()
+        HorizontalDivider(
+            thickness = 1.dp,
+            color = Color(0xFFB1B1B1)
+        )
+        OperationCustom(
+            R.drawable.ic_add_project,
+            text = "Danh sách mới",
+            modifier = Modifier
+                .padding(top = 8.dp)
+                .clickable { onAddNewProject() }
+        )
+        OperationCustom(
+            R.drawable.ic_edit,
+            text = "Chỉnh sửa danh sách",
+            modifier = Modifier
+                .clickable { onEditProject() }
+        )
+        OperationCustom(
+            R.drawable.ic_tag,
+            text = "Xem nhiệm vụ theo loại nhãn",
+            modifier = Modifier
+                .clickable { onProjectViewByTag() }
+        )
+        OperationCustom(
+            R.drawable.ic_lock,
+            text = "Xem nhiệm vụ đến hạn",
+            modifier = Modifier
+                .clickable { onProjectViewByDueDate() }
+        )
+        OperationCustom(
+            R.drawable.ic_countdown,
+            text = "Chế độ tập trung",
+            modifier = Modifier
+                .padding(bottom = 8.dp)
+                .clickable { onPomodoroMode() }
+        )
+        HorizontalDivider(
+            thickness = 1.dp,
+            color = Color(0xFFB1B1B1)
+        )
+        OperationCustom(
+            R.drawable.ic_full_tag,
+            text = "Nhãn",
+            modifier = Modifier
+                .padding(top = 8.dp)
+                .clickable { onTag() }
+        )
+        OperationCustom(
+            R.drawable.ic_move,
+            text = "Sắp xếp vị trí",
+            modifier = Modifier
+                .clickable { onMoveProject() }
+        )
+        OperationCustom(
+            R.drawable.ic_trash,
+            text = "Các mục đã xóa",
+            modifier = Modifier
+                .padding(bottom = 8.dp)
+                .clickable { onDeletedProjectView()}
+        )
+        HorizontalDivider(
+            thickness = 1.dp,
+            color = Color(0xFFB1B1B1)
+        )
     }
 }
 
 @Preview
 @Composable
 fun DrawerScreenPreview() {
-    DrawerScreen()
+    DrawerScreen({}, {}, {}, {}, {}, {}, {}, {}, {})
 }
