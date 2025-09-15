4942e35d0f8d72bf050cc4b386c2a90515c15871
PhamThiPhuongThanh36 <phamthiphuongthanh36@gmail.com>
2025-09-15 14:33:55 +0700

    add Activity

---
+++ app/src/main/java/com/example/todoapp/ui/main/ProjectDueDateActivity.kt
@@ -0,0 +1,21 @@
+package com.example.todoapp.ui.main
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun ProjectDueDateScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF009688))
+            .statusBarsPadding()
+    ) {
+
+    }
+}
---
+++ app/src/main/java/com/example/todoapp/ui/main/ProjectViewByTagActivity.kt
@@ -0,0 +1,21 @@
+package com.example.todoapp.ui.main
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun ProjectViewByTagScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFFFC107))
+            .statusBarsPadding()
+    ) {
+
+    }
+}
