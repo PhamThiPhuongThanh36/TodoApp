610eed0541e0247ffb3e06ab67f46b840325bc44
PhamThiPhuongThanh36 <phamthiphuongthanh36@gmail.com>
2025-09-15 14:33:30 +0700

    update MainActivity.kt

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -8,14 +8,17 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavGraph
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.createGraph
-import com.example.todoapp.ui.HomeScreen
+import com.example.todoapp.ui.main.HomeScreen
 import com.example.todoapp.ui.theme.ToDoAppTheme
+import com.example.todoapp.viewmodel.ListViewModel
+import com.example.todoapp.viewmodel.ProjectViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -33,19 +36,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
+    val projectViewModel: ProjectViewModel = hiltViewModel()
+    val listViewModel: ListViewModel = hiltViewModel()
     val navController = rememberNavController()
     Box(
         modifier = Modifier
             .fillMaxSize()
     ) {
-        NavHost(navController = navController, graph = navGraph(navController))
+        HomeScreen(projectViewModel)
     }
 }
 
-fun navGraph(navController: NavController): NavGraph {
-    return navController.createGraph(startDestination = "homeScreen") {
-        composable("homeScreen") {
-            HomeScreen()
-        }
-    }
-}
