ca32139a0c59b8bacfe25983e063bd071a35ef45
PhamThiPhuongThanh36 <phamthiphuongthanh36@gmail.com>
2025-09-15 14:33:19 +0700

    update Operation.kt

--- app/src/main/java/com/example/todoapp/ui/common/Operation.kt
+++ app/src/main/java/com/example/todoapp/ui/common/Operation.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -18,6 +19,7 @@ import androidx.compose.ui.unit.sp
 @Composable
 fun OperationCustom(icon: Int, text: String, modifier: Modifier = Modifier) {
     Row(
+        verticalAlignment = Alignment.CenterVertically,
         modifier = modifier
             .fillMaxWidth()
             .padding(top = 8.dp, bottom = 8.dp)
